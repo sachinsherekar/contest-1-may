e90e53eb7618b7270ef1d13814f4f25c8144c823
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-05-13 23:47:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function (student){
+     if(student.marks>50){
+      console.log(student);
+     }
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student)=>{
+    if(student.marks>50){
+      console.log(student);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let student= { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(student);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr=arr.filter(function(student){
+     if(student.marks>50){
+         return student;
+     }
+  })
+ console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let secondArr=[
+    {id:101, name:"Kalvin", age:"20", marks:60},
+    {id:115, name:"James", age:"21", marks:47},
+    {id:123, name:"Rocky", age:"19", marks:76},
+  ]
+   // let studentArr=arr.concat(secondArr);
+   // console.log(studentArr);
+  arr=arr.concat(secondArr);
+  console.log(arr);
 }
